d860e319883ac36218a4317b35f4b8f3dd019a59
Lastik30Bona <mhristov25@gmail.com>
2025-04-02 19:56:28 +0300

    Add project files.

---
+++ WikiSearchTest/SearchTest.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace WikiSearchTest
+{
+    public class Tests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void Search_Should_DisplayRelevantPage()
+        {
+            driver.Navigate().GoToUrl("https://wikipedia.org");
+
+            var wiki = new WikipediaPage(driver);
+            wiki.Search("Automation");
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            wait.Until(driver => driver.Title.Contains("Automation"));
+
+
+            Assert.That(driver.Title, Does.Contain("Automation"));
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+    }
+}
---
+++ WikiSearchTest/WikipediaPage.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+
+namespace WikiSearchTest
+{
+    public class WikipediaPage
+    {
+        private IWebDriver driver;
+
+        public WikipediaPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public IWebElement SearchInput => driver.FindElement(By.Id("searchInput"));
+
+        public void Search(string text)
+        {
+            SearchInput.SendKeys(text + Keys.Enter);
+        }
+    }
+
+}
